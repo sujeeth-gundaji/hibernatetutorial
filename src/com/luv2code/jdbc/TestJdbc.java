21abb73a3876ee5e641ede311f6ae34b3e1c7b7b
Sujeeth Gundaji <sujeeth.gundaji@gmail.com>
2017-10-05 15:21:09 -0500

    First commit

---
+++ src/com/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,26 @@
+package com.luv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/database";
+		String user = "root";
+		String pass = "mysql";
+
+		try {
+			System.out.println("Connecting to database:" + jdbcUrl);
+			Connection myconn = DriverManager
+					.getConnection(jdbcUrl, user, pass);
+			System.out.println("Connection succesfull!!!");
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
